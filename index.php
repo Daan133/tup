3300554375d1b011307ce5bdb46c6e3b0dd440ac
Daan133 <daan.wolthuis@ichthuslyceum.nl>
2026-06-09 19:09:08 +0000

    ja

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+
+<html lang="nl">
+
+<head>
+
+<title>Formula-1</title>
+
+<meta charset="utf-8">
+
+<meta name="author" content="Daan133">
+
+<meta name="keywords" content="template,HTML,metatags">
+
+<meta name="description" content="This is an fromula-1 template">
+
+<link rel="stylesheet" type="text/css" href="style.css">
+
+</head>
+
+<body>
+
+<header>
+    <h1>Formula-1</h1>
+</header>
+
+<nav>
+    <ul>
+        <p><a href="drivers.php">Drivers</a></p>
+        <p><a href="teams.php">Teams</a></p>
+        <p><a href="grandprix.php">Grand Prix</a></p>
+        <p><a href="standings.php">Standings</a></p>
+        <p><a href="contact.php">Contact</a></p>
+        <p><a href="cars.php">About the Cars</a></p>
+        <p><a href="history.php">History of Formula-1</a></p>
+        <p><a href="rules.php">Rules and Regulations</a></p>
+    </ul>
+</nav>
+
+</body>
+
+</html>

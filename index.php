165a618d4fb2e0b8152587cc8cee63bb66986acd
Daan133 <daan.wolthuis@ichthuslyceum.nl>
2026-06-10 08:21:33 +0000

    ja.

--- index.php
+++ index.php
@@ -34,8 +34,14 @@
         <p><a href="cars.php">About the Cars</a></p>
         <p><a href="history.php">History of Formula-1</a></p>
         <p><a href="rules.php">Rules and Regulations</a></p>
+        <p><a href="news.php">Latest News</a></p>
     </ul>
 </nav>
+
+<main>
+    <h2>Welcome to the Formula-1 website!</h2>
+    <p>Here you can find all the information about the drivers, teams, grand prix, standings, and more. Stay updated with the latest news and results from the world of Formula-1 racing.</p>
+</main>
 
 </body>
 
